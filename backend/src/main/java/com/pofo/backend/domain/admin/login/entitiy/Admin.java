7a0b4846fc2f1913c36351174319a58c1e88c1a2
leesangeok <wyzmqkr11@naver.com>
2025-01-31 12:10:16 +0900

    Feat :ã Admin, AdminLoginHistory 엔티티 ì¶가

---
+++ backend/src/main/java/com/pofo/backend/domain/admin/login/entitiy/Admin.java
@@ -0,0 +1,39 @@
+package com.pofo.backend.domain.admin.login.entitiy;
+
+import jakarta.persistence.*;
+import lombok.*;
+import com.pofo.backend.common.jpa.entity.BaseTime;
+
+@Entity
+@Table(name = "admins")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Admin extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    @Column(name = "failure_count", nullable = false, columnDefinition = "int default 0")
+    private int failureCount = 0;
+
+
+    public enum Status {
+        ACTIVE,
+        INACTIVE
+    }
+}
