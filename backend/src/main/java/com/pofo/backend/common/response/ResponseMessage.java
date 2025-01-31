770dc4d3a83598eacc6bf98f97b5bc5dc944f016
loong6138 <loong6138@gmail.com>
2025-01-31 16:52:04 +0900

    Refactor: ResponseMessage 불 필요한 메서드 삭제

--- backend/src/main/java/com/pofo/backend/common/response/ResponseMessage.java
+++ backend/src/main/java/com/pofo/backend/common/response/ResponseMessage.java
@@ -5,8 +5,4 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ResponseMessage<T>(String message, String resultCode, T data) {
-    @JsonIgnore
-    public int getStatusCode() {
-        return Integer.parseInt(resultCode.split("-")[0]);
-    }
 }
