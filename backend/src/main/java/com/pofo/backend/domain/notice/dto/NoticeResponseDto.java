e155c5309b8e4de39782b078d4540d8374c29fc4
loong6138 <loong6138@gmail.com>
2025-02-01 17:59:02 +0900

    Refactor: NoticeResponseDto 필드 변수 수정

--- backend/src/main/java/com/pofo/backend/domain/notice/dto/NoticeResponseDto.java
+++ backend/src/main/java/com/pofo/backend/domain/notice/dto/NoticeResponseDto.java
@@ -1,23 +1,13 @@
 package com.pofo.backend.domain.notice.dto;
 
-import java.time.LocalDateTime;
-
 import com.pofo.backend.domain.notice.entity.Notice;
 import lombok.*;
 
 @Getter
 public class NoticeResponseDto {
-    private Long id;
-    private String subject;
-    private String content;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    private Long responseId;
 
     public NoticeResponseDto(Notice notice) {
-        this.id = notice.getId();
-        this.subject = notice.getSubject();
-        this.content = notice.getContent();
-        this.createdAt = notice.getCreatedAt();
-        this.updatedAt = notice.getUpdatedAt();
+        this.responseId = notice.getId();
     }
 }
