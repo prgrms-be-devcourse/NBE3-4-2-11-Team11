ed7c3afbec6b55ddbdf9549938242aef9462ba30
sangxxjin <sj_0368@naver.com>
2025-02-02 17:44:44 +0900

    feat: ResumeResponse

---
+++ backend/src/main/java/com/pofo/backend/domain/resume/dto/ResumeResponse.java
@@ -0,0 +1,13 @@
+package com.pofo.backend.domain.resume.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ResumeResponse {
+    Long id;
+    String message;
+
+}
