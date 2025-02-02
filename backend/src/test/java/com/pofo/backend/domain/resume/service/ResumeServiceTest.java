da89db5c232e01315d9032e2e10298d70a29479a
sangxxjin <sj_0368@naver.com>
2025-02-02 18:50:20 +0900

    test: 이력서 생성 실패 - 사용자 정보 없음 구현
    본문:
    mock으로 만들었던 user를 생성하지 않고 생성해서 실패 메세지 확인하는 테스트 구현했습니다.

--- backend/src/test/java/com/pofo/backend/domain/resume/service/ResumeServiceTest.java
+++ backend/src/test/java/com/pofo/backend/domain/resume/service/ResumeServiceTest.java
@@ -1,10 +1,12 @@
 package com.pofo.backend.domain.resume.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
-import com.pofo.backend.domain.resume.dto.response.ResumeCreateResponse;
 import com.pofo.backend.domain.resume.dto.request.ResumeCreateRequest;
+import com.pofo.backend.domain.resume.dto.response.ResumeCreateResponse;
+import com.pofo.backend.domain.resume.exception.ResumeCreationException;
 import com.pofo.backend.domain.user.entity.User;
 import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,7 +31,7 @@ class ResumeServiceTest {
     }
 
     @Test
-    @DisplayName("이력서 생성 테스트")
+    @DisplayName("이력서 생성 성공")
     void createResume() {
         ResumeCreateRequest resumeCreateRequest = new ResumeCreateRequest();
         resumeCreateRequest.setName("김상진");
@@ -42,5 +44,26 @@ class ResumeServiceTest {
 
         ResumeCreateResponse response = resumeService.createResume(resumeCreateRequest, mockUser);
         assertEquals("이력서 생성이 완료되었습니다.", response.getMessage());
+    }
+
+    @Test
+    @DisplayName("이력서 생성 실패 - 사용자 정보 없음")
+    void createResumeWithNullUser() {
+        when(mockUser.getId()).thenReturn(null);
+
+        ResumeCreateRequest resumeCreateRequest = new ResumeCreateRequest();
+        resumeCreateRequest.setName("김상진");
+        resumeCreateRequest.setBirth(LocalDate.of(2000, 11, 18));
+        resumeCreateRequest.setNumber("010-1234-5678");
+        resumeCreateRequest.setEmail("prgrms@naver.com");
+        resumeCreateRequest.setAddress("서울시 강남구");
+        resumeCreateRequest.setGitAddress("https://github.com/kim");
+        resumeCreateRequest.setBlogAddress("https://kim.blog");
+
+        try {
+            resumeService.createResume(resumeCreateRequest, mockUser);
+        } catch (ResumeCreationException e) {
+            assertEquals("사용자 정보가 존재하지 않습니다.", e.getMessage());
+        }
     }
 }
