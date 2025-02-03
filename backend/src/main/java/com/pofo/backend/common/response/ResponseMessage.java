b0fc27cf4b8dd3428b4fa89ea6b23b26ac7d8534
loong6138 <loong6138@gmail.com>
2025-02-03 21:46:04 +0900

    Delete: 불 필요한 파일 삭제

--- backend/src/main/java/com/pofo/backend/common/response/ResponseMessage.java
+++
@@ -1,7 +0,0 @@
-package com.pofo.backend.common.response;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public record ResponseMessage<T>(String message, String resultCode, T data) {
-}
--- backend/src/main/java/com/pofo/backend/domain/notice/exception/NoticeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.pofo.backend.domain.notice.exception;
-
-public class NoticeNotFoundException extends RuntimeException {
-	public NoticeNotFoundException(String message) {
-		super(message);
-	}
-}
