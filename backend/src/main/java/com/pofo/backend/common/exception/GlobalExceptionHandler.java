15a9ce07cb17f9822382cb02871e67c789875d86
loong6138 <loong6138@gmail.com>
2025-02-03 21:47:33 +0900

    Refactor: NoticeException 으로 예외 처리 방식 변경

--- backend/src/main/java/com/pofo/backend/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/pofo/backend/common/exception/GlobalExceptionHandler.java
@@ -1,19 +1,18 @@
 package com.pofo.backend.common.exception;
 
+import com.pofo.backend.common.rsData.RsData;
+import com.pofo.backend.domain.notice.exception.NoticeException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.pofo.backend.common.rsData.RsData;
-import com.pofo.backend.domain.notice.exception.NoticeNotFoundException;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(NoticeNotFoundException.class)
+	@ExceptionHandler(NoticeException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public RsData<Object> handleNoticeNotFoundException(NoticeNotFoundException e) {
+	public RsData<Object> handleNoticeException(NoticeException e) {
 		return new RsData<>("404", e.getMessage());
 	}
 }
