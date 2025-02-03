412cc71d13e22d06f4f8d7e168eada85568d6bb3
leesangeok <wyzmqkr11@naver.com>
2025-02-03 14:22:49 +0900

    SecurityConfig 수정

--- backend/src/main/java/com/pofo/backend/common/security/SecurityConfig.java
+++ backend/src/main/java/com/pofo/backend/common/security/SecurityConfig.java
@@ -1,14 +1,44 @@
 package com.pofo.backend.common.security;
 
+import com.pofo.backend.common.security.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final TokenProvider tokenProvider;
+    private final RedisTemplate<String,String> redisTemplate;
+
+
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.formLogin().disable()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .authorizeRequests()
+                .requestMatchers("/api/v1/admin/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+
+                .apply(new JwtSecurityConfig(tokenProvider,redisTemplate))
+                .and().build();
+
+    }
+
 }
