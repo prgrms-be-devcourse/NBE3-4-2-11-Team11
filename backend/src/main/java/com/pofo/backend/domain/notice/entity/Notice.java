2019163fed671f6a2672675fe787fd65194d992a
loong6138 <loong6138@gmail.com>
2025-02-03 22:38:13 +0900

    Style: Notice 엔티티 Admin 부분 주석 처리

--- backend/src/main/java/com/pofo/backend/domain/notice/entity/Notice.java
+++ backend/src/main/java/com/pofo/backend/domain/notice/entity/Notice.java
@@ -3,6 +3,7 @@ package com.pofo.backend.domain.notice.entity;
 import com.pofo.backend.common.jpa.entity.BaseTime;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.transaction.annotation.Transactional;
 
 @Entity
 @Getter
@@ -12,9 +13,9 @@ import lombok.*;
 @Table(name = "notices")
 public class Notice extends BaseTime {
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "admin_id", nullable = false)
-    private Admin admin;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "admin_id", nullable = true)
+//    private Admin admin;
 
     @Column(length = 100, nullable = false)
     private String subject;
@@ -22,6 +23,7 @@ public class Notice extends BaseTime {
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
+    @Transactional
     public void update(String subject, String content) {
         this.subject = subject;
         this.content = content;
