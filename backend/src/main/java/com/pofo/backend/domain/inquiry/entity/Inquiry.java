715388b4d3f691231c3ad5ebd6c29a7e9fb4a571
loong6138 <loong6138@gmail.com>
2025-02-03 10:34:03 +0900

    Feat: Inquiry 엔티티 생성

---
+++ backend/src/main/java/com/pofo/backend/domain/inquiry/entity/Inquiry.java
@@ -0,0 +1,28 @@
+package com.pofo.backend.domain.inquiry.entity;
+
+import com.pofo.backend.common.jpa.entity.BaseTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "inquiries")
+public class Inquiry extends BaseTime {
+
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "user_id", nullable = false)
+	// private User user;
+
+	@Column(length = 100, nullable = false)
+	private String subject;
+
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String content;
+
+	@Column(nullable = false, columnDefinition = "TINYINT DEFAULT 0") // 문의사항 답변 상태
+	private int response;
+}
