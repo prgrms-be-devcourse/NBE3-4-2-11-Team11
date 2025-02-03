05b12f5d81e4abc36488afbe1cec4e07ae1fa9aa
sangxxjin <sj_0368@naver.com>
2025-02-03 16:13:13 +0900

    fix: merge conflict

--- backend/src/main/java/com/pofo/backend/domain/resume/resume/controller/ResumeController.java
+++ backend/src/main/java/com/pofo/backend/domain/resume/resume/controller/ResumeController.java
@@ -1,17 +1,16 @@
 package com.pofo.backend.domain.resume.resume.controller;
 
 import com.pofo.backend.common.rsData.RsData;
-import com.pofo.backend.domain.resume.resume.dto.request.ResumeCreateRequest;
-import com.pofo.backend.domain.resume.resume.dto.response.ResumeCreateResponse;
 import com.pofo.backend.domain.resume.resume.dto.response.ResumeResponse;
+import com.pofo.backend.domain.resume.resume.exception.ResumeCreationException;
 import com.pofo.backend.domain.resume.resume.service.ResumeService;
 import com.pofo.backend.domain.user.entity.User;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +22,9 @@ public class ResumeController {
     private final ResumeService resumeService;
 
     @PostMapping()
-    public RsData<Long> createResume(@Valid @RequestBody ResumeCreateRequest resumeCreateRequest, @AuthenticationPrincipal User user) {
-        ResumeCreateResponse resumeCreateResponse = resumeService.createResume(resumeCreateRequest, user);
-        return new RsData<>("200", resumeCreateResponse.getMessage(), resumeCreateResponse.getId());
+    public ResponseEntity<RsData<Object>> handleResumeCreationException(ResumeCreationException e) {
+        RsData<Object> rsData = new RsData<>("500", e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(rsData);
     }
 
     @GetMapping("/resume")
