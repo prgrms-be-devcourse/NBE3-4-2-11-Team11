a57066f83fcbc92a38f3827ddbe684e762f31f2d
sangxxjin <sj_0368@naver.com>
2025-02-03 15:14:51 +0900

    feat: 응답값 형식 통일

--- backend/src/main/java/com/pofo/backend/domain/resume/controller/ResumeController.java
+++ backend/src/main/java/com/pofo/backend/domain/resume/controller/ResumeController.java
@@ -3,10 +3,12 @@ package com.pofo.backend.domain.resume.controller;
 import com.pofo.backend.common.rsData.RsData;
 import com.pofo.backend.domain.resume.dto.request.ResumeCreateRequest;
 import com.pofo.backend.domain.resume.dto.response.ResumeCreateResponse;
+import com.pofo.backend.domain.resume.dto.response.ResumeIdResponse;
 import com.pofo.backend.domain.resume.service.ResumeService;
 import com.pofo.backend.domain.user.entity.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,8 +23,9 @@ public class ResumeController {
     private final ResumeService resumeService;
 
     @PostMapping()
-    public RsData<Long> createResume(@Valid @RequestBody ResumeCreateRequest resumeCreateRequest, @AuthenticationPrincipal User user) {
+    public ResponseEntity<RsData<ResumeIdResponse>> createResume(@Valid @RequestBody ResumeCreateRequest resumeCreateRequest, @AuthenticationPrincipal User user) {
         ResumeCreateResponse resumeCreateResponse = resumeService.createResume(resumeCreateRequest, user);
-        return new RsData<>("200", resumeCreateResponse.getMessage(), resumeCreateResponse.getId());
+        ResumeIdResponse resumeIdResponse = new ResumeIdResponse(resumeCreateResponse.getId());
+        return ResponseEntity.ok(new RsData<>("200", resumeCreateResponse.getMessage(),resumeIdResponse));
     }
 }
---
+++ backend/src/main/java/com/pofo/backend/domain/resume/dto/response/ResumeIdResponse.java
@@ -0,0 +1,10 @@
+package com.pofo.backend.domain.resume.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ResumeIdResponse {
+    Long id;
+}
