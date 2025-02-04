84730cad52698926a53f71aecdc2840b66d15e5c
sangxxjin <sj_0368@naver.com>
2025-02-04 15:40:05 +0900

    feat: activity repository

---
+++ backend/src/main/java/com/pofo/backend/domain/resume/activity/activity/repository/ActivityRepository.java
@@ -0,0 +1,12 @@
+package com.pofo.backend.domain.resume.activity.activity.repository;
+
+import com.pofo.backend.domain.resume.activity.activity.entity.Activity;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+    void deleteByResumeId(Long resumeId);
+
+    List<Activity> findByResumeId(Long resumeId);
+}
