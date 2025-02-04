4b04cd2fdf7b5dc3d4c52f075c1ca4fe3220e54e
sangxxjin <sj_0368@naver.com>
2025-02-04 12:45:15 +0900

    refactor: request not blank

--- backend/src/main/java/com/pofo/backend/domain/resume/license/dto/LicenseRequest.java
+++ backend/src/main/java/com/pofo/backend/domain/resume/license/dto/LicenseRequest.java
@@ -1,5 +1,7 @@
 package com.pofo.backend.domain.resume.license.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,8 +9,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LicenseRequest {
+    @NotBlank
     private String name;
+    @NotBlank
     private String institution;
+    @NotNull
     private LocalDate certifiedDate;
 
 }
