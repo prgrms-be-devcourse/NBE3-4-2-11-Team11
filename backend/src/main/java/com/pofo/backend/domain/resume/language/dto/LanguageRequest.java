6575721344875a9ce4e7b417c185e3dbe05caa3f
sangxxjin <sj_0368@naver.com>
2025-02-04 12:44:28 +0900

    refactor: request not blank

--- backend/src/main/java/com/pofo/backend/domain/resume/language/dto/LanguageRequest.java
+++ backend/src/main/java/com/pofo/backend/domain/resume/language/dto/LanguageRequest.java
@@ -1,5 +1,7 @@
 package com.pofo.backend.domain.resume.language.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +9,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LanguageRequest {
+    @NotBlank
     private String language;
-    private String result;
-    private LocalDate certifiedDate;
+    @NotBlank
     private String name;
+    @NotBlank
+    private String result;
+    @NotNull
+    private LocalDate certifiedDate;
 
 }
