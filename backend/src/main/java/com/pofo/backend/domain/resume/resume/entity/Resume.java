474fc754e41cb2e2ed316988a3dcea9fc8c16a77
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-02-05 12:23:35 +0900

    refactor: service, controller 책임 분리

--- backend/src/main/java/com/pofo/backend/domain/resume/resume/entity/Resume.java
+++ backend/src/main/java/com/pofo/backend/domain/resume/resume/entity/Resume.java
@@ -2,12 +2,12 @@ package com.pofo.backend.domain.resume.resume.entity;
 
 import com.pofo.backend.common.jpa.entity.BaseTime;
 import com.pofo.backend.domain.resume.activity.activity.entity.Activity;
-import com.pofo.backend.domain.user.join.entity.User;
 import com.pofo.backend.domain.resume.course.entity.Course;
-import com.pofo.backend.domain.resume.experience.entity.Experience;
 import com.pofo.backend.domain.resume.education.entity.Education;
-import com.pofo.backend.domain.resume.license.entity.License;
+import com.pofo.backend.domain.resume.experience.entity.Experience;
 import com.pofo.backend.domain.resume.language.entity.Language;
+import com.pofo.backend.domain.resume.license.entity.License;
+import com.pofo.backend.domain.user.join.entity.User;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- backend/src/main/java/com/pofo/backend/domain/resume/resume/service/ResumeService.java
+++ backend/src/main/java/com/pofo/backend/domain/resume/resume/service/ResumeService.java
@@ -1,33 +1,27 @@
 package com.pofo.backend.domain.resume.resume.service;
 
-
 import com.pofo.backend.domain.resume.activity.activity.service.ActivityService;
 import com.pofo.backend.domain.resume.course.service.CourseService;
-import com.pofo.backend.domain.resume.experience.service.ExperienceService;
 import com.pofo.backend.domain.resume.education.service.EducationService;
+import com.pofo.backend.domain.resume.experience.service.ExperienceService;
+import com.pofo.backend.domain.resume.language.service.LanguageService;
 import com.pofo.backend.domain.resume.license.service.LicenseService;
 import com.pofo.backend.domain.resume.resume.dto.request.ResumeCreateRequest;
-import com.pofo.backend.domain.resume.resume.dto.response.ResumeCreateResponse;
-import com.pofo.backend.domain.resume.resume.dto.response.ResumeResponse;
 import com.pofo.backend.domain.resume.resume.entity.Resume;
 import com.pofo.backend.domain.resume.resume.exception.ResumeCreationException;
 import com.pofo.backend.domain.resume.resume.exception.UnauthorizedActionException;
-import com.pofo.backend.domain.resume.resume.mapper.ResumeMapper;
 import com.pofo.backend.domain.resume.resume.repository.ResumeRepository;
-
 import com.pofo.backend.domain.user.join.entity.User;
-import com.pofo.backend.domain.resume.language.service.LanguageService;
-import jakarta.transaction.Transactional;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ResumeService {
 
     private final ResumeRepository resumeRepository;
-    private final ResumeMapper resumeMapper;
     private final ActivityService activityService;
     private final CourseService courseService;
     private final ExperienceService experienceService;
@@ -36,30 +30,21 @@ public class ResumeService {
     private final LanguageService languageService;
 
     @Transactional
-    public ResumeCreateResponse createResume(ResumeCreateRequest resumeCreateRequest, User user) {
-        Resume resume = buildResume(resumeCreateRequest, user);
-
-        resume = saveResumeAndRelatedEntities(resume, resumeCreateRequest);
-
-        return new ResumeCreateResponse(resume.getId(), "이력서 생성이 완료되었습니다.");
+    public Resume createResume(ResumeCreateRequest request, User user) {
+        Resume resume = buildResume(request, user);
+        return saveResumeAndRelatedEntities(resume, request);
     }
 
     @Transactional
-    public ResumeCreateResponse updateResume(Long resumeId, ResumeCreateRequest resumeCreateRequest,
-        User user) {
+    public Resume updateResume(Long resumeId, ResumeCreateRequest request, User user) {
         Resume resume = findResumeByIdAndCheckOwnership(resumeId, user);
-
-        updateResumeFields(resume, resumeCreateRequest);
-
-        resume = saveResumeAndRelatedEntities(resume, resumeCreateRequest);
-
-        return new ResumeCreateResponse(resume.getId(), "이력서 수정이 완료되었습니다.");
+        updateResumeFields(resume, request);
+        return saveResumeAndRelatedEntities(resume, request);
     }
 
     @Transactional
     public void deleteResume(Long resumeId, User user) {
         Resume resume = findResumeByIdAndCheckOwnership(resumeId, user);
-
         try {
             resumeRepository.delete(resume);
         } catch (DataAccessException e) {
@@ -67,15 +52,10 @@ public class ResumeService {
         }
     }
 
-    @Transactional
-    public ResumeResponse getResumeByUser(User user) {
-        Resume resume = resumeRepository.findByUser(user)
+    @Transactional(readOnly = true)
+    public Resume getResumeByUser(User user) {
+        return resumeRepository.findResumeWithDetails(user)
             .orElseThrow(() -> new ResumeCreationException("이력서가 존재하지 않습니다."));
-
-        ResumeResponse resumeResponse = resumeMapper.resumeToResumeResponse(resume);
-        populateResumeDetails(resumeResponse, resume.getId());
-
-        return resumeResponse;
     }
 
     private Resume buildResume(ResumeCreateRequest request, User user) {
@@ -103,22 +83,22 @@ public class ResumeService {
 
     private void addRelatedEntities(Resume resume, ResumeCreateRequest request) {
         if (request.getActivities() != null) {
-            activityService.addActivities(resume.getId(), request.getActivities());
+            activityService.updateActivities(resume.getId(), request.getActivities());
         }
         if (request.getCourses() != null) {
-            courseService.addCourses(resume.getId(), request.getCourses());
+            courseService.updateCourses(resume.getId(), request.getCourses());
         }
         if (request.getExperiences() != null) {
-            experienceService.addExperiences(resume.getId(), request.getExperiences());
+            experienceService.updateExperiences(resume.getId(), request.getExperiences());
         }
         if (request.getEducations() != null) {
-            educationService.addEducations(resume.getId(), request.getEducations());
+            educationService.updateEducations(resume.getId(), request.getEducations());
         }
         if (request.getLicenses() != null) {
-            licenseService.addLicenses(resume.getId(), request.getLicenses());
+            licenseService.updateLicenses(resume.getId(), request.getLicenses());
         }
         if (request.getLanguages() != null) {
-            languageService.addLanguages(resume.getId(), request.getLanguages());
+            languageService.updateLanguages(resume.getId(), request.getLanguages());
         }
     }
 
@@ -137,19 +117,9 @@ public class ResumeService {
     private Resume findResumeByIdAndCheckOwnership(Long resumeId, User user) {
         Resume resume = resumeRepository.findById(resumeId)
             .orElseThrow(() -> new ResumeCreationException("이력서가 존재하지 않습니다."));
-
         if (!resume.getUser().equals(user)) {
             throw new UnauthorizedActionException("이력서를 수정할 권한이 없습니다.");
         }
         return resume;
-    }
-
-    private void populateResumeDetails(ResumeResponse resumeResponse, Long resumeId) {
-        resumeResponse.setActivities(activityService.getActivitiesByResumeId(resumeId));
-        resumeResponse.setCourses(courseService.getCoursesByResumeId(resumeId));
-        resumeResponse.setExperiences(experienceService.getExperiencesByResumeId(resumeId));
-        resumeResponse.setEducations(educationService.getEducationsByResumeId(resumeId));
-        resumeResponse.setLicenses(licenseService.getLicensesByResumeId(resumeId));
-        resumeResponse.setLanguages(languageService.getLanguagesByResumeId(resumeId));
     }
 }
