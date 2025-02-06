922c1bbc2f5dd6c0afa7e5452705691e5dc358db
loong6138 <loong6138@gmail.com>
2025-02-06 16:01:13 +0900

    Feat: Test용 ReplyDetailResponseDto 추가

---
+++ backend/src/main/java/com/pofo/backend/domain/reply/dto/response/ReplyDetailResponse.java
@@ -0,0 +1,12 @@
+package com.pofo.backend.domain.reply.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReplyDetailResponse {
+
+    private Long id;
+    private String content;
+}
