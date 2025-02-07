82f4ec62a440bcf7d3f0b0ebd776d9b104a5ea71
pingu0118 <rnjsqhrud99@naver.com>
2025-02-08 05:06:36 +0900

    Feat: 공통에러처리

--- frontend/lib/fetch.ts
+++ frontend/lib/fetch.ts
@@ -9,4 +9,18 @@ const apiClient = axios.create({
 });
 
 
+// 응답 인터셉터 (공통 에러 처리) -> 특정api요청에서 별도의 추가 처리 필요하면 각 함수에 try-catch추가
+apiClient.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response) {
+        const message = error.response.data.message || '알 수 없는 오류가 발생했습니다.';
+        alert(`에러: ${message}`);
+      } else {
+        alert('서버와 연결할 수 없습니다.');
+      }
+      return Promise.reject(error);
+    }
+  );
+
 export default apiClient;
