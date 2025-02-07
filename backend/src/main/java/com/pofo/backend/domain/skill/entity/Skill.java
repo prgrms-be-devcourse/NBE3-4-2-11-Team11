01c2f7364159a27fe90c24ec300a9484a7bb4b75
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-02-08 07:41:53 +0900

    feat: skill entity

--- backend/src/main/java/com/pofo/backend/domain/skill/entity/Skill.java
+++ backend/src/main/java/com/pofo/backend/domain/skill/entity/Skill.java
@@ -1,26 +1,31 @@
 package com.pofo.backend.domain.skill.entity;
 
 import com.pofo.backend.common.jpa.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.*;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name="skills")
 @Getter
-@Setter
 public class Skill extends BaseEntity {
 
     @Column(nullable = false)
-    private String skill;
-
-
-    public Skill(long id, String skill) {
-        super();
-        this.skill = skill;
-    }
+    private String name;
+    @OneToMany(mappedBy = "skill", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ResumeSkill> resumeSkills = new ArrayList<>();
+    @OneToMany(mappedBy = "skill", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProjectSkill> projectSkills = new ArrayList<>();
 }
