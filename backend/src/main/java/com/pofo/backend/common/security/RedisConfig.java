1c92ee0241f0b86f8b18817e9e0e766642ebd2c4
loong6138 <loong6138@gmail.com>
2025-02-08 01:08:37 +0900

    Fix: redisTemplate @Primary 추가

--- backend/src/main/java/com/pofo/backend/common/security/RedisConfig.java
+++ backend/src/main/java/com/pofo/backend/common/security/RedisConfig.java
@@ -2,6 +2,7 @@ package com.pofo.backend.common.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -16,6 +17,7 @@ public class RedisConfig {
     }
 
     @Bean
+    @Primary
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
