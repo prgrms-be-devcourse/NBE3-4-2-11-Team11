71a9796e33ac399a8bc7e8242fd3b24aa9f6e4db
loong6138 <loong6138@gmail.com>
2025-02-07 14:43:21 +0900

    Feat: GlobalExceptionHandler 예외 추가

--- backend/src/main/java/com/pofo/backend/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/pofo/backend/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.pofo.backend.common.exception;
 
 import com.pofo.backend.common.rsData.RsData;
+import com.pofo.backend.domain.inquiry.exception.InquiryException;
 import com.pofo.backend.domain.notice.exception.NoticeException;
+import com.pofo.backend.domain.reply.exception.ReplyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +15,18 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(NoticeException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public RsData<Object> handleNoticeException(NoticeException e) {
+		return new RsData<>("404", e.getMessage());
+	}
+
+	@ExceptionHandler(InquiryException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public RsData<Object> handleInquiryException(InquiryException e) {
+		return new RsData<>("404", e.getMessage());
+	}
+
+	@ExceptionHandler(ReplyException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public RsData<Object> handleReplyException(ReplyException e) {
 		return new RsData<>("404", e.getMessage());
 	}
 }
