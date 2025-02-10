3799273dcc68755f296734e01fcde1cc537429f7
leesangeok <wyzmqkr11@naver.com>
2025-02-11 01:20:37 +0900

    fix : useTokenRefresh.ts 전역화

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css"; // 경로 확인 필수!
 import Header from "../components/Header";
+import TokenRefreshProvider from "../components/TokenRefreshProvider"; // 클라이언트 전용 컴포넌트
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,10 +21,12 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+
   return (
     <html lang="ko">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-100 text-gray-900`}>
         <Header />
+        <TokenRefreshProvider />
         <main className="container mx-auto min-h-screen px-4">{children}</main>
       </body>
     </html>
