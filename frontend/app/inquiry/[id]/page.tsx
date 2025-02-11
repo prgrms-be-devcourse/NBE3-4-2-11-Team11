cfc416b7697b1c92d509ce48f77cd636659ecacb
loong6138 <loong6138@gmail.com>
2025-02-11 16:32:07 +0900

    Feat: inquiry 상세 조회 페이지 구현

--- frontend/app/inquiry/[id]/page.tsx
+++ frontend/app/inquiry/[id]/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'next/navigation';
 import axios from 'axios';
-import styles from './InquiryDetailPage.module.css';
+import styles from './inquiryDetail.module.css';
 
 type ReplyDetailResponse = {
   // ReplyDetailResponse의 필드 정의
@@ -13,7 +14,6 @@ type InquiryDetailResponse = {
   id: number;
   subject: string;
   content: string;
-  response: number;
   createdAt: string; // LocalDateTime을 문자열로 변환
   reply: ReplyDetailResponse | null;
 };
@@ -24,31 +24,38 @@ type RsData<T> = {
   data: T;
 };
 
-const InquiryDetailPage = ({ params }) => {
+const InquiryDetailPage = () => {
+  const { id } = useParams();
   const [inquiry, setInquiry] = useState<InquiryDetailResponse | null>(null);
 
+
   useEffect(() => {
-    const fetchInquiryDetail = async () => {
+    if (id) {
+    const fetchInquiry = async () => {
+      console.log('Fetching inquiry detail for ID:', id); // ID 로그 출력
       try {
-        const response = await axios.get<RsData<InquiryDetailResponse>>(`/api/v1/common/inquiries/${params.id}`);
+        const response = await axios.get<RsData<InquiryDetailResponse>>(`/api/v1/common/inquiries/${id}`);
+        console.log('Response data:', response.data); // 디버깅 로그
         setInquiry(response.data.data);
       } catch (error) {
         console.error('Error fetching inquiry detail:', error);
       }
     };
 
-    fetchInquiryDetail();
-  }, [params.id]);
+    fetchInquiry();
+  }
+  }, [id]);
 
   if (!inquiry) return <div>Loading...</div>;
 
   return (
     <div className={styles.inquiryDetailContainer}>
-      <h1 className={styles.inquiryDetailHeader}>{inquiry.subject}</h1>
-      <p>{inquiry.content}</p>
-      <p>작성자 ID: {inquiry.userId}</p>
-      <p>응답 상태: {inquiry.response}</p>
-      <p>작성일: {new Date(inquiry.createdAt).toLocaleDateString('ko-KR')}</p>
+      <div className={styles.inquiryDetailSubjectRow}>
+        <h1 className={styles.inquiryDetailHeader}>{inquiry.subject}</h1>
+        <p className={styles.inquiryDetailDate}>{new Date(inquiry.createdAt).toLocaleDateString('ko-KR')}</p>
+      </div>
+      <hr className={styles.inquiryDetailDivider}/>
+      <p className={styles.inquiryDetailContent}>{inquiry.content}</p>
       {inquiry.reply && (
         <div>
           <h2>답변</h2>
