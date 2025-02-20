454527fad570d13cbc521b8f83cb640c7867f23a
loong6138 <loong6138@gmail.com>
2025-02-20 23:38:39 +0900

    Feat: CommentRepository 구현

---
+++ backend/src/main/java/com/pofo/backend/domain/comment/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.pofo.backend.domain.comment.repository;
+
+import com.pofo.backend.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
