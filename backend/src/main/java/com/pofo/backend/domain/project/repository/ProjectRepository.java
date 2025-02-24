ceaa1b8f8144312014c3aedc9ccbe769682748b1
seeyeon <gulf72pr@swu.ac.kr>
2025-02-25 01:40:19 +0900

    feat : 프로젝트 키워드 추가 - repository

--- backend/src/main/java/com/pofo/backend/domain/project/repository/ProjectRepository.java
+++ backend/src/main/java/com/pofo/backend/domain/project/repository/ProjectRepository.java
@@ -8,4 +8,7 @@ import java.util.List;
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
    List<Project>  findAllByOrderByIdDesc();
+
+   // 이름이나 설명에 키워드가 포함된 프로젝트 검색 (대소문자 구분 X)
+   List<Project> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String nameKeyword, String descriptionKeyword);
 }
