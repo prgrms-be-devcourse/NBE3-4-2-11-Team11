612d2ae3b2eef608123fd7b4617c9d7a7c496509
seeyeon <gulf72pr@swu.ac.kr>
2025-02-27 01:52:54 +0900

    feat : 다중 선택 삭제 및 휴지통 기능 추가

--- backend/src/main/java/com/pofo/backend/domain/project/repository/ProjectRepository.java
+++ backend/src/main/java/com/pofo/backend/domain/project/repository/ProjectRepository.java
@@ -1,7 +1,10 @@
 package com.pofo.backend.domain.project.repository;
 
 import com.pofo.backend.domain.project.entity.Project;
+import com.pofo.backend.domain.user.join.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +14,9 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
 
    // 이름이나 설명에 키워드가 포함된 프로젝트 검색 (대소문자 구분 X)
    List<Project> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String nameKeyword, String descriptionKeyword);
+
+   List<Project> findByUserAndIsDeletedTrue(User user);
+
+   @Query("SELECT p FROM Project p WHERE p.id IN :projectIds AND p.isDeleted = true")
+   List<Project> findAllByIdAndIsDeletedTrue(@Param("projectIds") List<Long> projectIds);
 }
