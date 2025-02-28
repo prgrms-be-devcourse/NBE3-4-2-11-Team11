f540ca251634b28f135c3d0be245f8269c844d5d
seeyeon <gulf72pr@swu.ac.kr>
2025-03-01 03:39:42 +0900

    feat : response에 프로젝트 썸네일 변수 추가

--- backend/src/main/java/com/pofo/backend/domain/project/dto/response/ProjectDetailResponse.java
+++ backend/src/main/java/com/pofo/backend/domain/project/dto/response/ProjectDetailResponse.java
@@ -1,12 +1,15 @@
 package com.pofo.backend.domain.project.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+@Builder
 @AllArgsConstructor
 @Getter
 @Setter
@@ -22,10 +25,14 @@ public class ProjectDetailResponse {
     private String repositoryLink;
     private String description;
     private String imageUrl;
+    private String thumbnailPath;
 
     // 기술 및 도구 목록 추가
-    private List<String> skills;
-    private List<String> tools;
+    @Builder.Default
+    private List<String> skills = new ArrayList<>();
+
+    @Builder.Default
+    private List<String> tools= new ArrayList<>();
 
     private boolean isDeleted;
 }
