d6ba76f34aed306012b969dd12dc75fff130f7f6
loong6138 <loong6138@gmail.com>
2025-03-04 16:12:07 +0900

    Fix: user 문의글 수정 기능 토큰 localStorage > cookie로 변경

--- frontend/app/inquiry/edit/[id]/page.tsx
+++ frontend/app/inquiry/edit/[id]/page.tsx
@@ -4,28 +4,23 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useParams } from "next/navigation";
-import { InquiryUpdateResponse } from './types'; // InquiryUpdateResponse 타입을 추가해야 합니다.
+
+type InquiryUpdateResponse = {
+    id: number;
+};
 
 const InquiryEditPage = () => {
   const { id } = useParams();
   const [inquiry, setInquiry] = useState<{ subject: string; content: string }>({ subject: "", content: "" });
   const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState<string | null>(null); // token 상태 정의
   const router = useRouter();
 
   useEffect(() => {
     const fetchInquiry = async () => {
-      const token = localStorage.getItem('accessToken');
-      if (!token) {
-        console.error('No access token found');
-        return;
-      }
 
       try {
-        const response = await axios.get<InquiryUpdateResponse>(`/api/v1/common/inquiries/${id}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const response = await axios.get<InquiryUpdateResponse>(`/api/v1/common/inquiries/${id}`, { withCredentials: true });
         setInquiry(response.data.data);
       } catch (error) {
         console.error('Error fetching inquiry:', error);
@@ -38,19 +33,9 @@ const InquiryEditPage = () => {
   }, [id]);
 
   const handleUpdate = async (updatedData) => {
-    const token = localStorage.getItem('accessToken');
-    if (!token) {
-      console.error('No access token found');
-      alert('로그인이 필요합니다.');
-      return;
-    }
 
     try {
-      const response = await axios.patch(`/api/v1/user/inquiries/${id}`, updatedData, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await axios.patch(`/api/v1/user/inquiries/${id}`, updatedData, { withCredentials: true });
       alert('문의사항이 성공적으로 수정되었습니다!');
       router.push(`/inquiry/${id}`);
     } catch (error) {
